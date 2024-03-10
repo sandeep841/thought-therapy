581442e1da2f9c4e9c03039e54a06c80fd6e380c
sandeep841 <sandeepkonuri444@gmail.com>
2024-03-11 02:02:13 +0530

    questionire added and options

---
+++ frontend/application/src/components/DepressionQuestions.js
@@ -0,0 +1,20 @@
+// DepressionQuestions.js
+const depressionQuestions = [
+        "I couldn't seem to experience any positive feeling at all", 
+        "I just couldn't seem to get going",
+        "I felt that I had nothing to look forward to",
+        "I felt sad and depressed",
+        "I felt that I had lost interest in just about everything",
+        "I felt I wasn't worth much as a person", 
+        "I felt that life wasn't worthwhile",
+        "I couldn't seem to get any enjoyment out of the things I did", 
+        "I felt down-hearted and blue", 
+        "I was unable to become enthusiastic about anything", 
+        "I felt I was pretty worthless", 
+        "I could see nothing in the future to be hopeful about",
+        "I felt that life was meaningless", 
+        "I found it difficult to work up the initiative to do things "
+  ];
+  
+  export default depressionQuestions;
+  
--- frontend/application/src/components/DepressionTherapy.js
+++ frontend/application/src/components/DepressionTherapy.js
@@ -1,11 +1,85 @@
 // DepressionTherapy.js
-import React from 'react';
+import React, { useState } from 'react';
+import depressionQuestions from '../components/DepressionQuestions';
 
 const DepressionTherapy = () => {
+  const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
+  const [userResponses, setUserResponses] = useState(Array(depressionQuestions.length).fill(''));
+
+  const handleNextQuestion = () => {
+    if (currentQuestionIndex < depressionQuestions.length - 1) {
+      setCurrentQuestionIndex(currentQuestionIndex + 1);
+    }
+  };
+
+  const handlePreviousQuestion = () => {
+    if (currentQuestionIndex > 0) {
+      setCurrentQuestionIndex(currentQuestionIndex - 1);
+    }
+  };
+
+  const handleOptionSelect = (option) => {
+    const updatedUserResponses = [...userResponses];
+    updatedUserResponses[currentQuestionIndex] = option;
+    setUserResponses(updatedUserResponses);
+  };
+
   return (
     <div>
-      <h1>Depression Therapy</h1>
-      {/* Add specific content for Depression Therapy */}
+      <h1>Depression Therapy - Question {currentQuestionIndex + 1}</h1>
+      <p>{depressionQuestions[currentQuestionIndex]}</p>
+
+      {/* Options */}
+      <div>
+        <label>
+          <input
+            type="radio"
+            value="Did not apply to me at all"
+            checked={userResponses[currentQuestionIndex] === 'Did not apply to me at all'}
+            onChange={() => handleOptionSelect('Did not apply to me at all')}
+          />
+          Did not apply to me at all
+        </label>
+        <label>
+          <input
+            type="radio"
+            value="Applied to me to some degree, or some of the time"
+            checked={userResponses[currentQuestionIndex] === 'Applied to me to some degree, or some of the time'}
+            onChange={() => handleOptionSelect('Applied to me to some degree, or some of the time')}
+          />
+          Applied to me to some degree, or some of the time
+        </label>
+        <label>
+          <input
+            type="radio"
+            value="Applied to me to a considerable degree, or a good part of the time"
+            checked={
+              userResponses[currentQuestionIndex] === 'Applied to me to a considerable degree, or a good part of the time'
+            }
+            onChange={() => handleOptionSelect('Applied to me to a considerable degree, or a good part of the time')}
+          />
+          Applied to me to a considerable degree, or a good part of the time
+        </label>
+        <label>
+          <input
+            type="radio"
+            value="Applied to me very much, or most of the time"
+            checked={userResponses[currentQuestionIndex] === 'Applied to me very much, or most of the time'}
+            onChange={() => handleOptionSelect('Applied to me very much, or most of the time')}
+          />
+          Applied to me very much, or most of the time
+        </label>
+      </div>
+
+      {/* Navigation buttons */}
+      <div>
+        <button onClick={handlePreviousQuestion} disabled={currentQuestionIndex === 0}>
+          Previous
+        </button>
+        <button onClick={handleNextQuestion} disabled={currentQuestionIndex === depressionQuestions.length - 1}>
+          Next
+        </button>
+      </div>
     </div>
   );
 };
